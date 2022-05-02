86e695fa13d4e704f0d5d20aef0fc565a15d014a
Muchori Joseph <muchorijoseph97@gmail.com>
2022-05-02 14:44:04 +0300

    Query Parameters

--- app/database.py
+++ app/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:$%(muchori97)@db.dcwjrqbghffburtaocxi.supabase.co:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -17,3 +18,12 @@ def get_db():
         yield db
     finally:
         db.close()
+
+### connecting to db using psycopg2
+
+# try:
+#   conn = psycopg2.connect(user='postgres', password='$%(muchori97)', host='db.dcwjrqbghffburtaocxi.supabase.co', port='5432', 
+#   database='postgres', cursor_factory=RealDictCursor)
+#   cursor = conn.cursor()
+# except Exception as error:
+#   print("Connect to a database: ", error)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
-import psycopg2
-from psycopg2.extras import RealDictCursor
-
 from fastapi import FastAPI
 
 from .import models
@@ -11,13 +8,6 @@ from .routers import post, user, auth
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-try:
-  conn = psycopg2.connect(user='postgres', password='$%(muchori97)', host='db.dcwjrqbghffburtaocxi.supabase.co', port='5432', 
-  database='postgres', cursor_factory=RealDictCursor)
-  cursor = conn.cursor()
-except Exception as error:
-  print("Connect to a database: ", error)
 
 app.include_router(post.router)
 app.include_router(user.router)
