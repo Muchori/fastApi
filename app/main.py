f90408328b50f8afa4bb6270354c0c53d0f3eee3
Muchori Joseph <36577265+Muchori@users.noreply.github.com>
2022-06-23 20:01:01 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -20,12 +20,10 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
-
 
 @app.get("/")
 def root():
